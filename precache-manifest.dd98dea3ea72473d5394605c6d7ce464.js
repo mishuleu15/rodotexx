1afa217fbbf33f997635b7daf8c6cea25d200b0b
mihaiCatalin15 <atudosieacatalin@gmail.com>
2020-09-02 21:47:23 +0300

    Updates

---
+++ precache-manifest.dd98dea3ea72473d5394605c6d7ce464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe779065c0734bbc178c43089bc7cb8",
+    "url": "/rodotexx/index.html"
+  },
+  {
+    "revision": "c07aacda77541ec612af",
+    "url": "/rodotexx/static/css/main.9210957c.chunk.css"
+  },
+  {
+    "revision": "41367084ea8312893b58",
+    "url": "/rodotexx/static/js/2.51cb50b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rodotexx/static/js/2.51cb50b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c07aacda77541ec612af",
+    "url": "/rodotexx/static/js/main.fe73c3ff.chunk.js"
+  },
+  {
+    "revision": "bbe517b7127b38e9eccb",
+    "url": "/rodotexx/static/js/runtime-main.9da51548.js"
+  }
+]);
